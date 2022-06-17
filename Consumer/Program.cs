501eadbaeff57fdb9d68648ecfc0cd4109921332
Marcos Eckart <mhme2000@hotmail.com>
2022-06-17 15:56:11 -0400

    Configuração inicial producer e consumer do rabbitmq

---
+++ Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// criando instância das configurações da conexão
+ConnectionFactory factory = new ConnectionFactory();
+factory.UserName = "guest"; //usuário padrão
+factory.Password = "guest"; //senha padrão
+factory.VirtualHost = "/";
+factory.HostName = "localhost";
+factory.Port = 5672; // porta padrão
+// criando a conexão de fato, com as configurações passadas acima
+IConnection conn = factory.CreateConnection();
+
+// criando o canal de comunicação dentro da conexão estabelecida
+IModel channel = conn.CreateModel();
+// declarando um gerenciador de filas (exchange)
+channel.ExchangeDeclare(exchange: "exchange_csharp", type: ExchangeType.Direct, durable: false, autoDelete: false, null);
+// declarando uma fila (queue)
+channel.QueueDeclare(queue: "queue_csharp", durable: true, exclusive: false, autoDelete: false, null);
+// declarando uma ligação entre o gerenciador (exchange) e a fila (queue)
+channel.QueueBind(queue: "queue_csharp", exchange: "exchange_csharp", routingKey: "key_csharp", null);
+
+// criando consumidor através do canal configurado anteriormente
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (ch, ea) =>
+{
+    var body = ea.Body.ToArray(); //pegando corpo da requisiçaõ recebida em bytes
+    var messageSerialize = Encoding.UTF8.GetString(body); //extraindo mensagem do corpo da requisição
+    var message = JsonSerializer.Deserialize<object>(messageSerialize); //desserializando mensagem em um objeto
+    Console.WriteLine(" Recebendo a seguinte mensagem: {0}", message);
+    channel.BasicAck(ea.DeliveryTag, false);
+    // aguardando 4 segundos para pegar a próxima mensagem da fila
+    Thread.Sleep(4000);
+};
+
+// consumindo mensagens da fila de acordo com as configurações passadas acima
+string consumerTag = channel.BasicConsume(queue: "queue_csharp", autoAck: false, consumer: consumer);
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using System.Text.Json;
+
+// criando instância das configurações da conexão
+ConnectionFactory factory = new ConnectionFactory();
+factory.UserName = "guest"; //usuário padrão
+factory.Password = "guest"; //senha padrão
+factory.VirtualHost = "/";
+factory.HostName = "localhost";
+factory.Port = 5672; // porta padrão
+// criando a conexão de fato, com as configurações passadas acima
+IConnection conn = factory.CreateConnection();
+
+// criando o canal de comunicação dentro da conexão estabelecida
+IModel channel = conn.CreateModel();
+// declarando um gerenciador de filas (exchange)
+channel.ExchangeDeclare(exchange: "exchange_csharp", type: ExchangeType.Direct, durable: false, autoDelete: false, null);
+// declarando uma fila (queue)
+channel.QueueDeclare(queue: "queue_csharp", durable: true, exclusive: false, autoDelete: false, null);
+// declarando uma ligação entre o gerenciador (exchange) e a fila (queue)
+channel.QueueBind(queue: "queue_csharp", exchange: "exchange_csharp", routingKey: "key_csharp", null);
+
+// criando e serializando mensagem em formato json
+var messageJson = new
+{
+    Type = "Sucesso",
+    Message = "Deu certo"
+};
+var messageJsonSerialize = JsonSerializer.Serialize(messageJson);
+var body = Encoding.UTF8.GetBytes(messageJsonSerialize);
+
+// enviando mensagem através do canal criado anteriormente
+while (true)
+{
+    channel.BasicPublish(exchange: "exchange_csharp", routingKey: "key_csharp", mandatory: false, null, body);
+    Console.WriteLine(" Enviando a seguinte mensagem: {0}", messageJsonSerialize);
+    // aguardando 2 segundos para enviar a próxima mensagem para a fila
+    Thread.Sleep(2000);
+}
